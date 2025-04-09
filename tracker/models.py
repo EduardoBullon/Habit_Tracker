54aee84dc696e593ad73fd5e152ee30b14e64d46
Eduardo <eduardobullonvera@gmail.com>
2025-04-09 14:52:38 -0500

    Funcionalidades de login, creación de superusuario y mejoras en las vistas

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Hábito(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    frecuencia = models.CharField(max_length=20, choices=[('diario', 'Diario'), ('semanal', 'Semanal')])
+
+    def __str__(self):
+        return self.nombre
+
+class RegistroHábito(models.Model):
+    hábito = models.ForeignKey(Hábito, related_name='registros', on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now_add=True)
+    completado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.hábito.nombre} - {self.fecha} - {'Completado' if self.completado else 'Pendiente'}"
